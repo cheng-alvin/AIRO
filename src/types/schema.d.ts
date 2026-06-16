8b038c2f1e2f7253231ef51e85b1af2a95f3e2b5
Alvin Cheng <eventide1029@gmail.com>
2026-06-16 14:42:39 +1000

    Added type information for address components

---
+++ src/types/schema.d.ts
@@ -0,0 +1,10 @@
+namespace AIRO {
+	interface addressComponentObject {
+		streetNumber: string
+		streetName: string
+		streetTypeLong: string
+		suburb: string
+		postcode: string
+		state: string
+	}
+}
